5c5d6e5b1bc70960ba386ed7691812dd6d22022d
lisahuele <lisahuele@gmail.com>
2021-09-14 22:00:19 -0400

    linking react with apollo

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,29 @@
-const typeDefs = {
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
 
-}
+const typeDefs = gql`
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        forSale: String
+        link: String
+        title: String
+    }
 
-module.exports = resolvers;
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]        
+    }
+    
+    type Query {
+        me: User
+    }
+`;
+
+module.exports = typeDefs;
