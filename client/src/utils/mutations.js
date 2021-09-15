189c05442d9c1397e886121e4f76524c78b39969
lisahuele <lisahuele@gmail.com>
2021-09-15 14:11:51 -0400

    login form

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,7 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-    mutation login($username: String!, $email: String!, $password: String!) {
+    mutation addUser($username: String!, $email: String!, $password: String!) {
         login(username: $username, email: $email, password: $password) {
             token
             user {
