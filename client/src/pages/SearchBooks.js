d773417ee1d2b64d9ab97a96d301bb47beb7efc0
lisahuele <lisahuele@gmail.com>
2021-09-15 11:53:38 -0400

    savebook mutation used for handler

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import {SAVE_BOOK} from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
+  // use useQuery hook to make query request
+  const [saveBook] = useMutation(SAVE_BOOK);
+
+
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
@@ -65,11 +72,15 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      await saveBook({
+        variables: {book: bookToSave},
+        update: cache => {
+          const {me} = cache.readQuery({ query: GET_ME })
+          console.log(me);
+          console.log(me.savedBooks);
+          cache.writeQuery({ query: GET_ME, data: {me: {...me, savedBooks: [...me.savedBooks, bookToSave] }}})
+        }
+      });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
